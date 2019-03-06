c7e5495c446601358f57141b9b00f90b08bad181
Devon Hockley <devon@hockley.ca>
2019-03-06 11:35:56 -0700

    Added project to git

---
+++ src/main/java/com/cpsc471/group26/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cpsc471.group26.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}

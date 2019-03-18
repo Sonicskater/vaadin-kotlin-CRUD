7d83f650b30f3cd383cce42102bacab868e08427
DESKTOP-IDR67U8\devon <devon@hockley.ca>
2019-03-18 14:22:55 -0600

    Uploaded new Gradle Project

---
+++ src/main/kotlin/com/cpsc471/demo/TheatreManagmentApplication.kt
@@ -0,0 +1,11 @@
+package com.cpsc471.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheatreManagmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<TheatreManagmentApplication>(*args)
+}
---
+++ src/test/kotlin/com/cpsc471/demo/TheatreManagmentApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.cpsc471.demo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TheatreManagmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
